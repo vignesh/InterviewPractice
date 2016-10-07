dd297218be646e22015e2ad3a46696da12565083
Vignesh Palaniappan <vignesh.palani96@gmail.com>
2016-10-07 11:57:29 -0700

    stringToInt

--- strings.cpp
+++ strings.cpp
@@ -2,16 +2,23 @@
 #include <string>
 using namespace std;
 
-int stringToInteger(char input) {
-	bool Negative = false;
+int stringToInteger(char *input) {
+	int sign = 1;
 	int i = 0;
-	if (input[i] == "-") {
-		Negative = true;
-		int i = 1;
+	int sum = 0;
+	if (input[i] == '-') {
+		sign = -1;
+		i = 1;
 	}
-
+	while (input[i] != '\0') {
+		//cout << i << " " << input[i] << " " << sum << endl;
+		sum = sum * 10 + input[i] - '0';
+		i++;
+	}
+	return (sum * sign);
 }
 
 int main() {
-
+	int value = stringToInteger("-100232");
+	cout << value << endl;
 }
