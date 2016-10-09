6d3d29997265a3b6413bc65a825a86ff1b117343
Vignesh Palaniappan <vignesh.palani96@gmail.com>
2016-10-08 17:30:52 -0700

    Nested Integer

--- nestedInteger.cpp
+++ nestedInteger.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <list>
 using namespace std;
 
+int depthSum(list<int> input, int level) {
+	int sum = 0;
+	int level = 1;
+	for (int i = 0; i < input.size(); i++) {
+		if (input[i].isInteger()) {
+			sum += input[i] * level;
+		}
+		else {
+			sum += depthSum(input[i].getList(), level + 1)
+		}
+	}
+	return sum;
+}
+
 int main() {
-	
+
 }
