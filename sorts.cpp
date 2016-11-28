dcdfbf84f5c4040f0c423f12abe846b9e1bbac04
Vignesh Palaniappan <vignesh.palani96@gmail.com>
2016-11-28 00:35:53 -0800

    Mergesort

--- sorts.cpp
+++ sorts.cpp
@@ -57,6 +57,58 @@ void quicksort(int data[], int left, int right) {
 }
 //O(nlogn), unstable, worst case O(n^2)
 
+void merge(int *a, int low, int high, int mid)
+{
+    int i, j, k, c[100];
+    i = low;
+    k = low;
+    j = mid + 1;
+    while (i <= mid && j <= high)
+    {
+        if (a[i] < a[j])
+        {
+            c[k] = a[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            c[k] = a[j];
+            k++;
+            j++;
+        }
+    }
+    while (i <= mid)
+    {
+        c[k] = a[i];
+        k++;
+        i++;
+    }
+    while (j <= high)
+    {
+        c[k] = a[j];
+        k++;
+        j++;
+    }
+    for (i = low; i < k; i++)
+    {
+        a[i] = c[i];
+    }
+}
+
+void mergesort(int *a, int low, int high)
+{
+    int mid;
+    if (low < high)
+    {
+        mid=(low+high)/2;
+        mergesort(a,low,mid);
+        mergesort(a,mid+1,high);
+        merge(a,low,high,mid);
+    }
+    return;
+}
+
 int main () {
 	int data [5] = {10, 20, 30, 5, 3}; 
 	quicksort(data, 0, 4);
