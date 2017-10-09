aa86c54f10a3a35839998ed17b9ad3f50e8ceba2
Vignesh Palaniappan <vignesh.palani96@gmail.com>
2017-10-08 18:13:55 -0700

    Merge sorted linked list

---
+++ LeetCode/mergeSortedLinkedLists.cpp
@@ -0,0 +1,19 @@
+Node  *mergeSortedLinkLists(Node *a, Node*b) {
+	Node * result = NULL
+	if (!a) {
+		return b;
+	}
+	if (!b) {
+		return a;
+	}
+
+	if (a->data > b->data) {
+		result = a;
+		return mergeSortedLinkedList(a->next, b);
+	}
+	if (a->data > b->data) {
+		result = b;
+		return mergeSortedLinkedList(a, b->next);
+	}
+	return result;
+}
