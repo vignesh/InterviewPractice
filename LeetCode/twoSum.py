fc60a39681716c43b041a3c282eee8c662fa706d
Vignesh Palaniappan <vignesh.palani96@gmail.com>
2017-10-20 00:38:06 -0700

    updated two sum

--- LeetCode/twoSum.py
+++ LeetCode/twoSum.py
@@ -17,3 +17,24 @@ class Solution(object):
             mapping[number] = counter
             counter+=1
         return finalArray
+
+
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        #given sorted array of numbers
+        l = 0
+        r = len(numbers)-1
+        while l < r:
+            sum2 = numbers[l] + numbers[r]
+            if sum2 == target:
+                #adding one to index, because anser requires not zero indexed
+                return [l+1,r+1]
+            elif target > sum2:
+                l+=1
+            else:
+                r-=1
